a7652d8770b750e3d9509c745a6a653cab5e18e3
MetoXen <jafarpatrik@gmail.com>
2023-03-14 17:32:45 +0100

    API testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.scss';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [currentWeather, setCurrentWather] = useState(null)
+
+  const loadData = async () => {
+    const ApiData = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=50.0515&longitude=14.2517&current_weather=true&timeyone=auto")
+    setCurrentWather(ApiData.data.current_weather)
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <h2>Prague Weather</h2>
+      {currentWeather && <div>Temp: {currentWeather.temperature}</div>}
     </div>
   );
 }
